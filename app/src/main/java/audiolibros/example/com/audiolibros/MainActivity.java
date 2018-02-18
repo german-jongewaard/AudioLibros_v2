cbdf310dbd8bf682dc016ee04db2d68781249fb6
german <germanjongewaard@gmail.com>
2018-02-18 02:02:09 +0100

    Agregando Fragments

--- app/src/main/java/audiolibros/example/com/audiolibros/MainActivity.java
+++ app/src/main/java/audiolibros/example/com/audiolibros/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView.setAdapter(adaptador);
+        adaptador = ((Aplicacion)getApplicationContext()).getAdaptador();
 
 
 
